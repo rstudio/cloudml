ce62eaacf57bd7a281eaf4d6249df47c7e1af0d8
Kevin Ushey <kevinushey@gmail.com>
2017-03-01 15:14:27 -0800

    delegate more responsibility to 'deploy.R'

--- inst/cloudml/cloudml/deploy.R
+++ inst/cloudml/cloudml/deploy.R
@@ -1,6 +1,13 @@
-# extract entrypoint
-arguments <- commandArgs(trailingOnly = TRUE)
+# extract command line arguments
+arguments <- as.list(commandArgs(trailingOnly = TRUE))
 entrypoint <- arguments[[1]]
+config     <- arguments[[2]]
+
+# set up environment
+Sys.setenv(
+  GCLOUD_EXECUTION_ENVIRONMENT = "1",
+  R_CONFIG_ACTIVE              = config
+)
 
 # read config overlay if available
 config <- list()
@@ -9,7 +16,7 @@ if (file.exists("cloudml/config.rds"))
 
 # generate filter to overlay this with config
 filter <- cloudml:::config_filter(config)
-config::add_filter(filter)
+uuid <- config::add_filter(filter)
 
 # source entrypoint
 source(entrypoint)
--- inst/cloudml/cloudml/deploy.py
+++ inst/cloudml/cloudml/deploy.py
@@ -3,25 +3,17 @@ import subprocess
 import sys
 import os
 
-# Extract command line arguments.
-entrypoint = sys.argv[1]
-config     = sys.argv[2]
-
-# Set up environment.
-os.environ["GCLOUD_EXECUTION_ENVIRONMENT"] = "1"
-os.environ["R_CONFIG_ACTIVE"] = config
-
 # Construct absolute path to 'deploy.R'.
 path, filename = os.path.split(os.path.realpath(__file__))
-entrypoint = os.path.realpath(os.path.join(path, "deploy.R"))
-if not os.path.exists(entrypoint):
-  raise IOError("Entrypoint '" + entrypoint + "' does not exist.")
+deploy = os.path.realpath(os.path.join(path, "deploy.R"))
+if not os.path.exists(deploy):
+  raise IOError("Entrypoint '" + deploy + "' does not exist.")
 
 # Move to the application directory.
 os.chdir(os.path.dirname(path))
 
 # Run 'Rscript' with this entrypoint. Forward command line arguments.
-commands = ["Rscript", entrypoint]
+commands = ["Rscript", deploy]
 [commands.append(argument) for argument in sys.argv[1:]]
 
 process = subprocess.Popen(
