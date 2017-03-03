3759e95b72129403ba52d1d389c37970922e02ef
JJ Allaire <jj@rstudio.org>
2017-03-03 10:50:27 -0500

    install cloudml

--- inst/cloudml/cloudml/deploy.R
+++ inst/cloudml/cloudml/deploy.R
@@ -2,8 +2,8 @@
 options(repos = c(CRAN = "http://cran.rstudio.com"))
 options(download.file.method = "wget")
 install.packages(c("devtools", "RCurl"))
-devtools::install_github("rstudio/config")
 devtools::install_github("rstudio/tensorflow")
+devtools::install_github("rstudio/cloudml")
 
 # extract command line arguments
 arguments <- as.list(commandArgs(trailingOnly = TRUE))
