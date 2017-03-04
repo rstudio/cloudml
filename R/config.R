29b1abac80c93cb4a838a37a15febe7ec0b2979e
JJ Allaire <jj@rstudio.org>
2017-03-04 14:55:38 -0500

    remove unused resolve_config function

--- R/config.R
+++ R/config.R
@@ -27,29 +27,3 @@ config_filter <- function(extra_config) {
   }
 }
 
-
-resolve_config <- function(key,
-                           value,
-                           config,
-                           config_path)
-{
-  # attempt to resolve 'key' as a child of node
-  # 'value' in configuration 'config'
-  configuration <- config::get(
-    value  = value,
-    config = config,
-    file   = config_path
-  )
-
-  setting <- configuration[[key]]
-  if (!is.null(setting))
-    return(setting)
-
-  # if that fails, attempt to resolve as a top-level
-  # child for configuration 'config'
-  config::get(
-    value  = key,
-    config = config,
-    file   = config_path
-  )
-}
