5317b04390c96e66f0d54bcfa80a29589e415ff9
Kevin Ushey <kevinushey@gmail.com>
2017-03-07 10:39:05 -0800

    clean up predict example

--- inst/examples/census/predict.R
+++ inst/examples/census/predict.R
@@ -2,14 +2,43 @@ library(tensorflow)
 
 source("model.R")
 
-# path to dataset used for prediction
-eval_file <- "local/gs/rstudio-cloudml-demo-ml/census/data/local.adult.data"
+# use test dataset for prediction
+filename <- "local/gs/rstudio-cloudml-demo-ml/census/data/local.adult.test"
 
-# define estimator
+# rebuild estimator from existing job directory
 estimator <- build_estimator("jobs/local")
 
-# use model to generate predictions with our input function
-input_fn <- generate_input_fn(eval_file)
-predictions <- iterate(estimator$predict(input_fn = input_fn))
-classes <- iterate(estimator$predict_classes(input_fn = input_fn))
+# generate input function (request only 1 epoch
+# so we just get one round of predictions)
+input_fn <- generate_input_fn(filename = filename,
+                              num_epochs = 1L,
+                              batch_size = 10L)
+
+# generate predictions
+predictions   <- iterate(estimator$predict(input_fn = input_fn))
+classes       <- iterate(estimator$predict_classes(input_fn = input_fn))
 probabilities <- iterate(estimator$predict_proba(input_fn = input_fn))
+
+# read in dataset and attach probabilities
+dataset <- read.table(
+  file = filename,
+  header = FALSE,
+  sep = ",",
+  col.names = CSV_COLUMNS
+)
+dataset$predicted_classes <- as.numeric(classes)
+dataset$predicted_probabilities <- as.numeric(lapply(probabilities, `[[`, 2))
+
+# generate some simple scatterplots
+library(ggplot2)
+
+gg <- ggplot(dataset, aes(x = occupation, y = predicted_probabilities)) +
+  geom_boxplot() +
+  coord_flip() +
+  labs(
+    x = "Occupation",
+    y = "P(Income Bracket > 50K)",
+    title = "P(Income Bracket > 50K) vs. Occupation"
+  )
+
+print(gg)
