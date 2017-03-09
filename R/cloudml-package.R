58263c93364e609027301920b6bea827d112b153
JJ Allaire <jj@rstudio.org>
2017-03-09 10:29:17 -0500

    remove package topic (we don't want this on the pkgdown reference page)

--- R/cloudml-package.R
+++ R/cloudml-package.R
@@ -1,9 +1,4 @@
-#' R Interface to Google Cloud ML
-#'
-#' This package provides an interface to Google's Cloud Machine Learning
-#' platform.
-#'
-"_PACKAGE"
+
 
 # package globals
 .globals <- new.env(parent = emptyenv())
