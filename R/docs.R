8201884b703d28f95f57e7e8b81dcf488e4caae2
JJ Allaire <jj@rstudio.org>
2017-03-10 16:33:55 -0500

    rename to doc-utils.R

--- R/docs.R
+++ R/docs.R
@@ -1,7 +1,8 @@
 
 
 docs_site <- function (input, encoding = getOption("encoding"), ...) {
-  source("docs.R", local = TRUE)
+  docs_site_impl <- function(...) {}
+  source("doc-utils.R", local = TRUE)
   docs_site_impl(input, encoding, ...)
 }
 
