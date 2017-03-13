2dbba79f28468c4e9417fb63a940d34c4fefe60a
Kevin Ushey <kevinushey@gmail.com>
2017-03-13 15:55:53 -0700

    move packrat snapshot call before '__init__' spray

--- R/gcloud-utils.R
+++ R/gcloud-utils.R
@@ -13,6 +13,22 @@ initialize_application <- function(application = getwd()) {
     "cloudml"
   )
 
+  # use packrat for deployment when specified
+  if (isTRUE(config$packrat_enabled)) {
+
+    # ignore 'cloudml' for now since it will usually be
+    # installed from source and we'll want to just reinstall
+    # from github anyhow -- we can probably change this later
+    packrat::opts$ignored.packages("cloudml")
+    packrat::.snapshotImpl(
+      project = getwd(),
+      ignore.stale = TRUE,
+      prompt = FALSE,
+      snapshot.sources = FALSE,
+      verbose = FALSE
+    )
+  }
+
   # ensure sub-directories contain an '__init__.py'
   # script, so that they're all included in tarball
   dirs <- list.dirs(application)
@@ -58,23 +74,6 @@ scope_deployment <- function(application = getwd(), config) {
   # move to application path
   owd <- setwd(deployment)
   defer(setwd(owd), envir = parent.frame())
-
-  # use packrat for deployment when specified
-  if (isTRUE(config$packrat_enabled)) {
-
-    # ignore 'cloudml' for now since it will usually be
-    # installed from source and we'll want to just reinstall
-    # from github anyhow -- we can probably change this later
-    packrat::opts$ignored.packages("cloudml")
-
-    packrat::.snapshotImpl(
-      project = getwd(),
-      ignore.stale = TRUE,
-      prompt = FALSE,
-      snapshot.sources = FALSE,
-      verbose = FALSE
-    )
-  }
 
   # return normalized application path
   deployment
