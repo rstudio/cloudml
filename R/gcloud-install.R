2f1c477e4dcd90b2c1aa33d191b5b65c9d6e5833
Javier Luraschi <javierluraschi@hotmail.com>
2017-11-03 21:53:35 -0700

    fix typo

--- R/gcloud-install.R
+++ R/gcloud-install.R
@@ -27,7 +27,7 @@ gcloud_path <- function() {
 
   candidates <- c(
     function() Sys.which("gcloud"),
-    function() "~/google-cloud-sdk/bin/gcloud"
+    function() "~/google-cloud-sdk/bin/gcloud",
     function() file.path(Sys.getenv("GCLOUD_INSTALL_PATH", "~/google-cloud-sdk"), "bin/gcloud")
   )
 
