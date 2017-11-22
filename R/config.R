66eae29a1aeeea679e85189c11698d0d415c9f99
Javier Luraschi <javierluraschi@hotmail.com>
2017-11-22 15:25:07 -0800

    implement cloudml_create_config() to assist creating cloudml.yml

--- R/config.R
+++ R/config.R
@@ -10,3 +10,34 @@ cloudml_config <- function(path = getwd()) {
   config$cloudml
 
 }
+
+#' Creates Default Configuration File
+#'
+#' Creates a default 'cloudml.yml' configuration file to simplify the
+#' configuration process. This file is created in the current directory,
+#' open this file to configure this further.
+#'
+#' @export
+cloudml_create_config <- function() {
+  if (file.exists("cloudml.yml")) {
+    warning("cloudml.yml already exists.")
+    return()
+  }
+
+  config_defaults <- list(
+    gcloud = list(
+      project = "project-name",
+      account = "account@domain.com",
+      region  = "us-central1",
+      "runtime-version" = "1.2"
+    ),
+    cloudml = list(
+      storage = "gs://project-name/mnist"
+    )
+  )
+
+  yaml_content <- yaml::as.yaml(config_defaults)
+  writeLines(yaml_content, "cloudml.yml")
+
+  invisible(NULL)
+}
