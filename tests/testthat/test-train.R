976394b9a739650c5101e4086eaf7f10d4d7c2f3
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-02 11:08:51 -0800

    add keras test

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -1,19 +1,6 @@
 context("train")
 
-test_that("cloudml_train() can train and collect savedmodel", {
-  if (!cloudml_tests_configured()) return()
-
-  config_yml <- system.file("examples/mnist/cloudml.yml", package = "cloudml")
-  file.copy("cloudml.yml", config_yml, overwrite = TRUE)
-
-  job <- cloudml_train(
-    application = system.file(
-      "examples/mnist/",
-      package = "cloudml"
-    ),
-    entrypoint = "train.R"
-  )
-
+expect_train_succeeds <- function(job) {
   expect_gt(nchar(job$id), 0)
   expect_gt(length(job$description), 0)
   expect_gt(nchar(job$description$state), 0)
@@ -29,4 +16,38 @@ test_that("cloudml_train() can train and collect savedmodel", {
     pattern = "saved_model")
 
   expect_gte(length(saved_model), 1)
+}
+
+test_that("cloudml_train() can train and collect savedmodel", {
+  if (!cloudml_tests_configured()) return()
+
+  config_yml <- system.file("examples/mnist/cloudml.yml", package = "cloudml")
+  file.copy("cloudml.yml", config_yml, overwrite = TRUE)
+
+  job <- cloudml_train(
+    application = system.file(
+      "examples/mnist/",
+      package = "cloudml"
+    ),
+    entrypoint = "train.R"
+  )
+
+  expect_train_succeeds(job)
+})
+
+test_that("cloudml_train() can train keras model", {
+  if (!cloudml_tests_configured()) return()
+
+  config_yml <- system.file("examples/keras/cloudml.yml", package = "cloudml")
+  file.copy("cloudml.yml", config_yml, overwrite = TRUE)
+
+  job <- cloudml_train(
+    application = system.file(
+      "examples/keras/",
+      package = "cloudml"
+    ),
+    entrypoint = "train.R"
+  )
+
+  expect_train_succeeds(job)
 })
