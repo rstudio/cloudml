c13f7717b5e9c0634cdf25440168836a31db0171
jjallaire <jj@rstudio.com>
2017-12-06 12:03:10 -0800

    update getting started vignette

--- R/config.R
+++ R/config.R
@@ -21,44 +21,4 @@ find_cloudml_config <- function(path = getwd()) {
 }
 
 
-#' Initializes Default Configuration File
-#'
-#' Creates a default 'cloudml.yml' configuration file to initialize the
-#' configuration process. This file is created in the current directory,
-#' open this file to configure this further.
-#'
-#' @export
-cloudml_init <- function(name, project = name, storage = paste0("gs://", project)) {
 
-  # check if the directory already exists
-  if (file_test("-d", name))
-    stop("Directory named '", name, "' already exists'")
-
-  # create directory
-  dir.create(name)
-  message(paste0("- Created project directory '", name, "'"))
-
-  # copy template training script
-  file.copy(system.file("templates", "train.R", package = "cloudml"),
-            to = name)
-  message("- Created training script 'train.R'")
-
-  # create config.yml file
-  cloudml_yml <- file.path(name, "cloudml.yml")
-  config_defaults <- list(
-    gcloud = list(
-      project = project
-    ),
-    cloudml = list(
-      storage = storage
-    )
-  )
-  yaml_content <- yaml::as.yaml(config_defaults)
-  writeLines(yaml_content, cloudml_yml)
-  message("- Created config file 'cloudml.yml':")
-  message(paste(paste("  ", readLines(cloudml_yml)), collapse = "\n"))
-
-
-  # return path invisibly
-  invisible(name)
-}
