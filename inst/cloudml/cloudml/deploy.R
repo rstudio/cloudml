a687ee0f489e8c61b84ac8b23558e4bc4068a3ea
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-06 17:17:44 -0800

    add support to cache ~/.keras data

--- inst/cloudml/cloudml/deploy.R
+++ inst/cloudml/cloudml/deploy.R
@@ -104,42 +104,35 @@ config <- yaml::yaml.load_file("cloudml.yml")
 cloudml <- config$cloudml
 
 cache <- cloudml[["cache"]]
-if (is.null(cache)) {
+cache_enabled <- !identical(cloudml[["cache"]], FALSE)
+
+if (!cache_enabled) {
   cache <- file.path(cloudml[["storage"]], "cache")
   message(paste0("Cache entry not found, defaulting to: ", cache))
 } else {
   message(paste0("Cache entry found: ", cache))
 }
 
-use_packrat <- cloudml[["packrat"]]
-if (is.null(use_packrat)) {
-  use_packrat <- TRUE
-}
+use_packrat <- !identical(cloudml[["packrat"]], FALSE)
 
-if (!identical(cache, FALSE)) {
-  cache <- file.path(cache, ifelse(use_packrat, "packrat", "r"))
-}
-
-get_cached_packages <- function () {
-  cached_entries <- system2("gsutil", c("ls", cache), stdout = TRUE)
+get_cached_bundles <- function (source) {
+  cached_entries <- system2("gsutil", c("ls", source), stdout = TRUE)
   as.character(lapply(strsplit(basename(cached_entries), "\\."), function(e) e[[1]]))
 }
 
-store_cached_packages <- function (cache_path) {
-  if (identical(cache, FALSE)) return()
-
-  cached_entries <- get_cached_packages()
+store_cached_data <- function (source, destination) {
+  cached_entries <- get_cached_bundles(destination)
   installed <- rownames(installed.packages())
 
-  for (pkg in dir(cache_path)) {
+  for (pkg in dir(source)) {
     if (!pkg %in% cached_entries) {
-      source <- file.path(cache_path, pkg)
+      source_entry <- file.path(source, pkg)
       compressed <- file.path(tempdir(), paste0(pkg, ".tar"))
 
       message(paste0("Compressing '", pkg, "' package to ", compressed, " cache."))
-      system2("tar", c("-cf", compressed, "-C", source, "."))
+      system2("tar", c("-cf", compressed, "-C", source_entry, "."))
 
-      target <- file.path(cache, paste0(pkg, ".tar"))
+      target <- file.path(destination, paste0(pkg, ".tar"))
 
       message(paste0("Adding '", compressed, "' to ", target, " cache."))
       system(paste("gsutil", "cp", shQuote(compressed), shQuote(target)))
@@ -147,13 +140,11 @@ store_cached_packages <- function (cache_path) {
   }
 }
 
-retrieve_cached_packages <- function(target) {
-  if (identical(cache, FALSE)) return()
-
-  compressed <- file.path(tempdir(), "cache/")
+retrieve_cached_data <- function(source, target) {
+  compressed <- tempfile()
   if (!file_test("-d", compressed)) dir.create(compressed, recursive = TRUE)
 
-  remote_path <- file.path(cache, "*")
+  remote_path <- file.path(source, "*")
 
   message(paste0("Retrieving packages from ", remote_path, " cache into ", compressed, "."))
   system(paste("gsutil", "-m", "cp", "-r", shQuote(remote_path), shQuote(compressed)))
@@ -190,16 +181,26 @@ retrieve_default_packages <- function() {
   }
 }
 
-cache_path <- if (use_packrat) tempfile() else .libPaths()[[1]]
-retrieve_cached_packages(cache_path);
+cache_local <- if (use_packrat) tempfile() else .libPaths()[[1]]
+cache_keras_local <- "~/.keras/"
+cache_remote <- file.path(cache, ifelse(use_packrat, "packrat", "r"))
+cache_keras_remote <- file.path(cache, "keras")
+
+if (cache_enabled) {
+  retrieve_cached_data(cache_remote, cache_local)
+  retrieve_cached_data(cache_keras_remote, cache_keras_local)
+}
 
 if (use_packrat) {
-  retrieve_packrat_packages(cache_path)
+  retrieve_packrat_packages(cache_local)
 } else {
   retrieve_default_packages()
 }
 
-store_cached_packages(cache_path)
+if (cache_enabled) {
+  store_cached_data(cache_local, cache_remote)
+  store_cached_data(cache_keras_local, cache_keras_remote)
+}
 
 # Training ----
 
