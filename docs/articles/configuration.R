02649348a4666146b642bcb97e4795af184d5b28
jjallaire <jj@rstudio.com>
2017-12-11 09:40:50 -0500

    regenerate website

---
+++ docs/articles/configuration.R
@@ -0,0 +1,7 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, eval=FALSE)
+
+## ----eval=F--------------------------------------------------------------
+#  library(cloudml)
+#  cloudml_create_config()
+
---
+++ docs/articles/tuning.R
@@ -0,0 +1,7 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, eval=FALSE)
+
+## ----eval=FALSE----------------------------------------------------------
+#  job <- cloudml_tune()
+#  job_collect(job)
+
