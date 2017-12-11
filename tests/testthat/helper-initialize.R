bdded29d83b67a6fb9d3ae5bcebe81b63f10f288
Javier Luraschi <javierluraschi@hotmail.com>
2017-12-11 13:14:35 -0800

    use zero config work to run tests locally

--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -20,15 +20,19 @@ sysenv_file <- function(name, destination) {
 
 cloudml_write_config <- function(destination = "cloudml.yml") {
   config = list(
-    gcloud = list(
-      project = Sys.getenv("GCLOUD_PROJECT"),
-      account = Sys.getenv("GCLOUD_ACCOUNT"),
-      region  = Sys.getenv("CLOUDSDK_COMPUTE_REGION", "us-east1")
-    ),
-    cloudml = list(
-      storage = paste("gs://", Sys.getenv("GCLOUD_PROJECT"), "/travis", sep = "")
-    )
+    gcloud = list(),
+    cloudml = list()
   )
+
+  if (nchar(Sys.getenv("GCLOUD_PROJECT")) > 0)
+    config$gcloud$project <- Sys.getenv("GCLOUD_PROJECT")
+
+  if (nchar(Sys.getenv("GCLOUD_ACCOUNT")) > 0)
+    config$gcloud$account <- Sys.getenv("GCLOUD_ACCOUNT")
+
+  if (nchar(Sys.getenv("GCLOUD_PROJECT")) > 0)
+    config$gcloud$storage <- paste("gs://", Sys.getenv("GCLOUD_PROJECT"), "/travis", sep = "")
+
   yaml::write_yaml(config, destination)
 }
 
--- tests/testthat/test-jobs.R
+++ tests/testthat/test-jobs.R
@@ -1,8 +1,6 @@
 context("jobs")
 
 test_that("job_list() succeeds", {
-  if (!cloudml_tests_configured()) return()
-
   all_jobs <- job_list()
   expect_gte(nrow(all_jobs), 0)
 })
