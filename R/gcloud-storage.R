4582030f23a24451a72e7da78749125f7c70fa31
jjallaire <jj@rstudio.com>
2017-12-12 09:52:13 -0500

    add gcloud_rsync function

--- R/gcloud-storage.R
+++ R/gcloud-storage.R
@@ -20,12 +20,63 @@
 #'   copying directories.
 #'
 #' @export
-gcloud_copy <- function(source, destination, recursive = FALSE, echo = FALSE) {
+gcloud_copy <- function(source, destination, recursive = FALSE, echo = TRUE) {
 
   arguments <- c(
     "-m",
     "cp",
     if (recursive) "-r",
+    source,
+    destination
+  )
+
+  gsutil_exec(args = arguments, echo = echo)
+}
+
+
+#' Synchronize content of two buckets/directories
+#'
+#' The `gcloud_rsync` function makes the contents under `destination` the same
+#' as the contents under `source`, by copying any missing files/objects (or
+#' those whose data has changed), and (if the `delete` option is specified)
+#' deleting any extra files/objects. `source` must specify a directory, bucket,
+#' or bucket subdirectory.
+#'
+#' @inheritParams gcloud_copy
+#'
+#' @param delete Delete extra files under `destination` not found under
+#'   `source` By default extra files are not deleted.
+#' @param recursive Causes directories, buckets, and bucket subdirectories to
+#'   be synchronized recursively. If you neglect to use this option
+#'   `gcloud_rsync()` will make only the top-level directory in the source and
+#'   destination URLs match, skipping any sub-directories.
+#' @param parallel Causes synchronization to run in parallel. This can
+#'   significantly improve performance if you are performing operations on a
+#'   large number of files over a reasonably fast network connection.
+#' @param dry_run Causes rsync to run in "dry run" mode, i.e., just outputting
+#'   what would be copied or deleted without actually doing any
+#'   copying/deleting.
+#' @param options Character vector of additional command line options to the
+#'   gsutil rsync command (as specified at
+#'   <https://cloud.google.com/storage/docs/gsutil/commands/rsync>).
+#'
+#' @export
+gcloud_rsync <- function(source, destination,
+                         delete = FALSE, recursive = FALSE,
+                         parallel = TRUE, dry_run = FALSE,
+                         options = NULL,
+                         echo = TRUE) {
+
+  if (!utils::file_test("-d", destination))
+    dir.create(destination, recursive = TRUE)
+
+  arguments <- c(
+    if (parallel) "-m",
+    "rsync",
+    if (delete) "-d",
+    if (recursive) "-r",
+    if (dry_run) "-n",
+    options,
     source,
     destination
   )
