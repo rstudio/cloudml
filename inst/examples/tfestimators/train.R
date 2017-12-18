e467eedec6c82437e6f08e944c43a65f03b612d9
jjallaire <jj@rstudio.com>
2017-12-18 16:02:05 -0500

    add tfestimators docs/example

---
+++ inst/examples/tfestimators/train.R
@@ -0,0 +1,34 @@
+
+
+library(tfestimators)
+
+mtcars_input_fn <- function(data, num_epochs = 1) {
+  input_fn(data,
+           features = c("disp", "cyl"),
+           response = "mpg",
+           batch_size = 32,
+           num_epochs = num_epochs)
+}
+
+cols <- feature_columns(
+  column_numeric("disp"),
+  column_numeric("cyl")
+)
+
+
+model <- linear_regressor(feature_columns = cols)
+
+indices <- sample(1:nrow(mtcars), size = 0.80 * nrow(mtcars))
+train <- mtcars[indices, ]
+test  <- mtcars[-indices, ]
+
+model %>% train(mtcars_input_fn(train, num_epochs = 10))
+
+model %>% evaluate(mtcars_input_fn(test))
+
+obs <- mtcars[1:3, ]
+model %>% predict(mtcars_input_fn(obs))
+
+export_savedmodel(model, export_dir_base = "savedmodel")
+
+
