d5d96a7cee4f5632774d6dbf7a96a35a9539e80c
Daniel Falbel <dfalbel@gmail.com>
2019-08-30 16:04:32 -0300

    add \r for Windows

--- R/gcloud-version.R
+++ R/gcloud-version.R
@@ -7,7 +7,8 @@
 #' @export
 gcloud_version <- function() {
   out <- gcloud_exec("version", echo = FALSE)
-  version <- strsplit(unlist(strsplit(out$stdout, "\n")), " (?=[^ ]+$)", perl = TRUE)
+  version <- strsplit(unlist(strsplit(out$stdout, "\r?\n")),
+                      " (?=[^ ]+$)", perl = TRUE)
 
   v_numbers <- lapply(version, function(x) numeric_version(x[2]))
   names(v_numbers) <- sapply(version, function(x) x[1])
